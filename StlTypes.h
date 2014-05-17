18d6f5b0f92ede8cd63c8db85201fc198b906715
amocsy <amocsy@gmail.com>
2014-05-17 19:00:10 +0200

    no need for exception :D

--- StlTypes.h
+++ StlTypes.h
@@ -10,8 +10,4 @@ namespace types
    unsigned _typeID<std::string>() { return 40; }
    template<>
    unsigned _typeID<std::string*>() { return 41; }
-   template<>
-   unsigned _typeID<std::exception>() { return 42; }
-   template<>
-   unsigned _typeID<std::exception*>() { return 43; }
 }
